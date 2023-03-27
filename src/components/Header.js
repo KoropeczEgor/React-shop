299c33a399859ea2673920b6366d415bcd14df73
Egor <koropeczegor@gmail.com>
2023-03-27 20:29:25 +0300

    creating a shopping cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { FaShoppingCart } from "react-icons/fa"
 
 export default function Header() {
+  let [cart0pen, setCartOpen] = useState(false)
+
   return (
     <header>
         <div>
@@ -10,6 +13,12 @@ export default function Header() {
                 <li>Контакты</li>
                 <li>Кабинет</li>
             </ul>
+            <FaShoppingCart onClick={() => setCartOpen(cart0pen = !cart0pen)} className={`shop-cart-bitton ${cart0pen && 'active'}`}/>
+
+            {cart0pen && (
+              <div className='shop-cart'>
+              </div>
+            )}
         </div>
         <div className='presedentation'></div>
     </header>
